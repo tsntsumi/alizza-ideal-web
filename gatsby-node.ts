49c26356ddda65e10fb329bbb2a4a3aac1c57f52
TSUTSUMI Kikuo <kikuo@alizza-ideal.com>
2021-08-06 05:36:22 +0900

    Reinstall plugins

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,14 +1,17 @@
-import { createFilePath } from 'gatsby-source-filesystem';
-import { GatsbyNode } from 'gatsby';
-import path from 'path';
+import { createFilePath } from "gatsby-source-filesystem"
+import { GatsbyNode, graphql } from "gatsby"
+import path from "path"
 
-
-export const onCreateNode: GatsbyNode['onCreateNode'] = async ({ node, getNode, actions }) => {
+export const onCreateNode: GatsbyNode["onCreateNode"] = async ({
+    node,
+    getNode,
+    actions,
+}) => {
     const { createNodeField } = actions
     if (node.internal.type === `Mdx`) {
         const slug = createFilePath({ node, getNode })
         const sourceName = getNode(node.parent).sourceInstanceName
-        const prefix = sourceName === "basepages" ? '' : '/'+sourceName;
+        const prefix = sourceName === "basepages" ? "" : "/" + sourceName
 
         createNodeField({
             node,
@@ -23,8 +26,10 @@ export const onCreateNode: GatsbyNode['onCreateNode'] = async ({ node, getNode, 
     }
 }
 
-
-export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
+export const createPages: GatsbyNode["createPages"] = async ({
+    graphql,
+    actions,
+}) => {
     const { createPage } = actions
 
     return graphql<any>(`
@@ -46,7 +51,9 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
                     }
                 }
             }
-            portfolio: allMdx(filter: { fields: { sourceName: { eq: "portfolio" } } }) {
+            portfolio: allMdx(
+                filter: { fields: { sourceName: { eq: "portfolio" } } }
+            ) {
                 edges {
                     node {
                         id
@@ -90,11 +97,12 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
             })
         })
 
-
         const portfolioItems = result.data.portfolio.edges
         const portfolioItemsPerPage =
             result.data.limitPost.siteMetadata.portfolioItemsPerPage
-        const numPortfolioItems = Math.ceil(portfolioItems.length / portfolioItemsPerPage)
+        const numPortfolioItems = Math.ceil(
+            portfolioItems.length / portfolioItemsPerPage
+        )
 
         Array.from({ length: numPortfolioItems }).forEach((_, i) => {
             createPage({
