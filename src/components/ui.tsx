85edac72a984cd49aa582ad45ae3d3da9e0910e8
TSUTSUMI Kikuo <kikuo@alizza-ideal.com>
2021-09-01 15:55:54 +0900

    Add Offer and Cta buttons

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react"
 import { Link } from "./utils"
 
-type ButtonProps = { title: string, to?: string, type?: any, label?: string, disabled?: boolean, iconLeft?: JSX.Element, iconRight?: JSX.Element };
+type ButtonProps = {
+    title: string
+    to?: string
+    type?: any
+    label?: string
+    disabled?: boolean
+    iconLeft?: JSX.Element
+    iconRight?: JSX.Element
+}
 const Button: React.FC<ButtonProps> = props => {
     const { title, to, type, label, disabled } = props
 
@@ -17,13 +25,20 @@ const Button: React.FC<ButtonProps> = props => {
         </React.Fragment>
     )
 
-
-    if(type) {
-        const b = type.split(",");
+    if (type) {
+        const b = type.split(",")
         const t = b[1] ? b[1] : "button"
-        const dis = disabled === undefined ? false: disabled
+        const dis = disabled === undefined ? false : disabled
         if (b[0] === "button") {
-            return <button type={t} disabled={dis} className={`btn btn-primary${dis ? ' disabled' : ''}`}>{innerComponents}</button>
+            return (
+                <button
+                    type={t}
+                    disabled={dis}
+                    className={`btn btn-primary${dis ? " disabled" : ""}`}
+                >
+                    {innerComponents}
+                </button>
+            )
         }
     }
     return (
@@ -55,9 +70,9 @@ const TextInput = ({ label, type = "text", name, onChange, footer }) => {
                 name={name}
                 onChange={event => {
                     event.target.style.height = "auto"
-                    event.target.style.height = event.target.scrollHeight + "px";
+                    event.target.style.height = event.target.scrollHeight + "px"
 
-                    onChange(event);
+                    onChange(event)
                 }}
                 onFocus={() => changeFocused(true)}
                 onBlur={() => changeFocused(false)}
@@ -73,14 +88,48 @@ const TextInput = ({ label, type = "text", name, onChange, footer }) => {
             } transition-all duration-300 py-3 lg:p-4 pb-6`}
         >
             <p className="text-color-3">{label}</p>
-            <div className="bg-gradient-primary p-2px">
-                {elem}
-            </div>
-            {footer && 
-            <>{footer}</>
-            }
+            <div className="bg-gradient-primary p-2px">{elem}</div>
+            {footer && <>{footer}</>}
         </div>
     )
 }
 
-export { Button, TextInput }
+type CtaProps = {
+    title: string
+    to?: string
+    disabled?: boolean
+    iconLeft?: JSX.Element
+    iconRight?: JSX.Element
+}
+const Cta: React.FC<ButtonProps> = props => {
+    const { title, to, disabled, iconLeft, iconRight } = props
+    return (
+        <center>
+            <Button
+                className="m-0"
+                type="button"
+                title={title}
+                iconLeft={iconLeft}
+                iconRight={iconRight}
+            >
+                <Button type="link" to={to} />
+            </Button>
+        </center>
+    )
+}
+
+const Offer = props => {
+    const { children, className, bgColor } = props
+
+    return (
+        <div
+            className={`border-4 border-double rounded-md border-blue-400 rounded-md px-8 py-4 my-8 text-black ${className ||
+                ""}`}
+            style={{ backgroundColor: bgColor || "#e6fffa" }}
+        >
+            {children}
+        </div>
+    )
+}
+
+export { Button, TextInput, Cta, Offer }
