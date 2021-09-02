49372d33733cc5fae0fb8f403252cd4a3e197fbc
TSUTSUMI Kikuo <kikuo@alizza-ideal.com>
2021-09-02 19:33:37 +0900

    Add template files for landing page generationg functions

---
+++ src/components/item-landingpage.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react"
+import Img from "gatsby-image"
+import recommended from "remark-preset-lint-recommended"
+import remarkHtml from "remark-html"
+import { ArrowRight } from "react-feather"
+import { LandingPageListQuery_allMdx_edges_node } from "../templates/__generated__/LandingPageListQuery"
+import { Button } from "../components/ui"
+import { Calendar } from "react-feather"
+import { IndexPageQuery_landingpage_edges_node } from "../pages/__generated__/IndexPageQuery"
+import { Link } from "gatsby"
+import { remark } from "remark"
+
+type ItemLandingPageProps =
+    | LandingPageListQuery_allMdx_edges_node
+    | IndexPageQuery_landingpage_edges_node
+export const ItemLandingPage: React.FC<{ data: ItemLandingPageProps }> = ({
+    data,
+}) => {
+    const [focused, changeFocused] = useState(false)
+    const description = remark()
+        .use(recommended)
+        .use(remarkHtml)
+        .processSync(data.frontmatter.description)
+        .toString()
+
+    return (
+        <div className="landingPage-item w-full md:w-1/2 lg:w-1/3 p-4">
+            <div
+                className={`transition-all duration-300 hover:shadow-2xl shadow ${focused &&
+                    "focused"}`}
+            >
+                <Link
+                    to={data.fields.slug}
+                    title={data.frontmatter.title}
+                    onFocus={() => changeFocused(true)}
+                    onBlur={() => changeFocused(false)}
+                >
+                    <div className="image">
+                        <Img
+                            fluid={data.frontmatter.image.childImageSharp.fluid}
+                            alt={data.frontmatter.title}
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="p-4 py-3">
+                        <h4 className="text-color-2 font-black text-3xl pt-1">
+                            {data.frontmatter.title}
+                        </h4>
+                        <div className="hidden flex items-center text-secondary">
+                            <Calendar className="stroke-current" />
+                            <p className="pl-2 text-color-default font-sans">
+                                {data.frontmatter.date}
+                            </p>
+                        </div>
+                        <div className="pt-3 text-color-default">
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: description,
+                                }}
+                            />
+                        </div>
+                    </div>
+                </Link>
+                <Button
+                    to={data.fields.slug}
+                    label={`View ${data.frontmatter.title}`}
+                    title={"View"}
+                    iconRight={<ArrowRight />}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default ItemLandingPage
