27d4222f36034a638bb3c4c906f2c9e6718f262b
TSUTSUMI Kikuo <kikuo@alizza-ideal.com>
2022-01-22 17:13:27 +0900

    Add more explain about google business profile

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -114,30 +114,17 @@ const plugins = [
                 {
                     resolve: `gatsby-remark-images`,
                     options: {
-                        maxWidth: 1200,
+                        maxWidth: 512,
+                        linkImagesToOriginal: true,
                     },
                 },
-            ],
-        },
-    },
-    {
-        resolve: "gatsby-transformer-remark",
-        options: {
-            plugins: [
                 {
-                    resolve: "gatsby-remark-embed-video",
+                    resolve: `gatsby-remark-embed-youtube`,
                     options: {
-                        width: 800,
-                        ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-                        height: 400, // Optional: Overrides optional.ratio
-                        related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-                        noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
-                        loadingStrategy: "lazy", //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
-                        containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
-                        iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
+                        width: 320,
+                        height: 240,
                     },
                 },
-                `gatsby-remark-images`,
                 `gatsby-remark-responsive-iframe`,
             ],
         },
