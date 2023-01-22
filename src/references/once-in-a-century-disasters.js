c42a59d2bf041f9ea2820bb8fb01b512f09bb108
TSUTSUMI Kikuo <kikuo@alizza-ideal.com>
2023-01-22 16:19:48 +0900

    Rename extension

--- src/references/once-in-a-century-disasters.js
+++ src/references/once-in-a-century-disasters.js
@@ -1,4 +1,4 @@
-// i18next-extract-mark-ns-start translation
+// i18next-extract-mark-ns-start common
 import * as React from "react"
 import styled from "styled-components"
 import { Trans } from "gatsby-plugin-react-i18next"
