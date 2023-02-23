edbe534c2f05b7ab00511e8c471e102c106c6316
TSUTSUMI Kikuo <kikuo@alizza-ideal.com>
2023-02-23 20:34:47 +0900

    Update to insert landing page styles tag

--- src/pages/kashakasha/trial-entry-form.js
+++ src/pages/kashakasha/trial-entry-form.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import { Trans, useI18next } from "gatsby-plugin-react-i18next"
 import "moment/locale/ja"
 import { GlobalStyle } from "../../components/layout/styles"
+import { LandingPageStyles } from "../../components/layout/landingpage-styles"
 import Seo from "../../components/seo"
 
 export const Head = ({ location, params, data, pageContext }) => {
@@ -25,38 +26,40 @@ const KashaKashaTrialEntryForm = ({ data, pageContext, location }) => {
   return (
     <>
       <GlobalStyle />
-      <h1 style={{ padding: "0.75em 0em" }}>
-        <span
-          style={{
-            color: "yellow",
-            whiteSpace: "nowrap",
-            fontWeight: 900,
-            fontSize: "1.6em",
-          }}
-        >
-          <Trans>Googleマップ集客術</Trans>
-        </span>
-        <br />
-        <span
-          style={{ color: "yellow", whiteSpace: "nowrap", fontSize: "0.8em" }}
-        >
-          <Trans>Googleマップアカウント開設</Trans>
-        </span>
-        <span
-          style={{ color: "yellow", whiteSpace: "nowrap", fontSize: "0.8em" }}
-        >
-          <Trans>個別体験会登録フォーム</Trans>
-        </span>
-      </h1>
-      <div id="entry-form" style={{ width: "100%", height: "100vh" }}>
-        <kls-form-embeded-tag>
-          <iframe
-            width="100%"
-            class="kls-sform-AsPe"
-            data-src="https://kli.jp/sf/AsPe/"
-          ></iframe>
-        </kls-form-embeded-tag>
-      </div>
+      <LandingPageStyles>
+        <h1 style={{ padding: "0.75em 0em" }}>
+          <span
+            style={{
+              color: "yellow",
+              whiteSpace: "nowrap",
+              fontWeight: 900,
+              fontSize: "1.6em",
+            }}
+          >
+            <Trans>Googleマップ集客術</Trans>
+          </span>
+          <br />
+          <span
+            style={{ color: "yellow", whiteSpace: "nowrap", fontSize: "0.8em" }}
+          >
+            <Trans>Googleマップアカウント開設</Trans>
+          </span>
+          <span
+            style={{ color: "yellow", whiteSpace: "nowrap", fontSize: "0.8em" }}
+          >
+            <Trans>個別体験会登録フォーム</Trans>
+          </span>
+        </h1>
+        <div style={{ width: "100%", height: "100vh" }}>
+          <kls-form-embeded-tag>
+            <iframe
+              width="100%"
+              class="kls-sform-AsPe"
+              data-src="https://kli.jp/sf/AsPe/"
+            ></iframe>
+          </kls-form-embeded-tag>
+        </div>
+      </LandingPageStyles>
     </>
   )
 }
